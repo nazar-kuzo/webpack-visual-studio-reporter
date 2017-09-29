1f66e08dda119dbd6d34b2f14b970221902b51c5
Nazar Kuzo <nazar.kuzo@outlook.com>
2017-09-29 07:22:06 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -3,33 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-class WebpackVisualStudioReporterPlugin {
-    constructor() {
-        this.plugin("done", stats => {
-            if (stats.compilation.errors) {
-                var output = "";
 
-                stats.compilation.errors.forEach(error => {
-                    var err = error.toString();
+var webpackVisualStudioReporter = require('./webpack-visual-studio-reporter');
 
-                    if (error.name == "Error") {
-                        try {
-                            output += err.match(/(\/.*?\.\S*)/)[0].split(":")[0].substr(1).replace(/\//g, "\\");
-                            output += ": line " + err.match(/(\/.*?\.\S*)/)[0].split(":")[1];
-                            output += ", col " + err.match(/(\/.*?\.\S*)/)[0].split(":")[2];
-                            output += ", " + error.message.split(/( {4}.*\d)[:]/)[2].trim().replace(/(\r\n|\r|\n)/g, "");
-                            output += " (" + error.message.match(/( {4}.*\d)[:]/)[1].trim() + ")\n";
-                        } catch (ex) {
-                            output += ".\\webpack.config.js: line 0, col 0, " + error.message + "\n";
-                        }
-                    } else {
-                        output += ".\\webpack.config.js: line 0, col 0, " + error.message + "\n";
-                    }
-                });
+Object.defineProperty(exports, 'WebpackVisualStudioReporter', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(webpackVisualStudioReporter).default;
+  }
+});
 
-                console.log(output);
-            }
-        });
-    }
-}
-exports.default = WebpackVisualStudioReporterPlugin;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
